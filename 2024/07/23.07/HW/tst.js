0240ff7dcf742a3e492fbe8bce7423cf11b9821c
Atlas1101 <123ayc@gmail.com>
2024-07-24 12:02:50 +0300

    Added tst.js and made other necessary changes

---
+++ 2024/07/23.07/HW/tst.js
@@ -0,0 +1,47 @@
+let firstName = "Aharon";
+
+let lastName = "Cohen";
+
+let age = 26;
+
+let isStudent = true;
+
+let isAdult = (age>=18);
+console.log("isAdult is", isAdult);
+
+let isJohn = firstName === "John";
+console.log("is John is",isJohn);
+console.log(`is John ${isJohn}`);
+
+
+function greeta(firstName, lastName){
+let fullName = firstName.toUpperCase() + " "+lastName.toUpperCase();
+return(`Hello ${fullName}! Welcome to the IITC Bootcamp`);
+}
+console.log(greeta("Aharon","Cohen"));
+
+
+function greet(firstName, lastName) {
+    let fullName = firstName + " " + lastName;
+    return "Hello "+ fullName +"! Welcome to the IITC Bootcamp." ;
+}
+let x= greet("aharon","cohen");
+console.log(x);
+
+
+function checkAge(age){
+if (age<13){
+  console.log("You are a child")
+
+}else if (age>=13<=17){
+  console.log("You are a teenager")
+
+  }else if(age>=18<=64){
+  console.log("You are an adult")
+
+  }else return("You are a senior")
+}
+console.log(checkAge(12))
+console.log(checkAge(17))
+console.log(checkAge(24))
+console.log(checkAge(90))
