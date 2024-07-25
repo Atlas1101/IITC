9bd9430e693765b26b12ec7d6b73b66c8b7a0984
Aharon Cohen <ayc@live.co.uk>
2024-07-25 16:03:07 +0300

    on to task 3

---
+++ 2024/07/25.07/HW.F/task 2.assignOper/main.js
@@ -0,0 +1,50 @@
+//1
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+
+//2
+// use "=" eg let snow = 19
+
+//3 Adds value to a variable
+
+//4   x +=5
+
+//5 Subtracts value from a variable
+
+//6   y-=3
+
+//7 it multiplies a Variable
+
+//8 z*=2
+
+//9 it divides a variable
+
+//10 w/=4
+
+//11 hey achieve the same result, but "+="" is more concise
+
+//12 w++, +=1
+
+//13
+
+let aString = "one"
+let bString = "two"
+let wonderWhatHappens = aString += bString
+console.log(wonderWhatHappens)
+
+//14
+
+let cString = "hello"
+let gString = " there"
+let iKnowWhatHappens = cString += gString
+console.log(iKnowWhatHappens)
+
+// 15
+let x = 5
+x+=3
+console.log(x);
