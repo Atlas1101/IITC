755c2a8b3c08c92e746a0382dcda61f27e93752a
Aharon Cohen <ayc@live.co.uk>
2024-07-25 15:16:49 +0300

    updated

---
+++ 2024/07/25.07/HW.F/task 1/script.js
@@ -0,0 +1,56 @@
+//JavaScript Arithmetic Operations Quiz
+
+//1
+
+let a = 5 + 3
+console.log(a);
+//2
+let aa = 4
+let b = 6
+console.log(aa + b);
+
+//3
+"-"
+//4
+let bb = 10-4
+console.log(bb);
+//5
+let c =2
+let d =3
+console.log(c*d);
+//6
+let cc = 6*2
+console.log(cc);
+//7
+"/"
+//8
+let dd = 15/3
+console.log(dd);
+//9
+let divByZero = 4/0
+console.log(divByZero);//infinity
+//10
+let remainder = 15%7
+console.log(remainder);
+//11
+let remainderA = 17%5
+console.log(remainderA);
+//12
+let score = 1
+while (score<=5 ){
+console.log(score);
+score++
+}
+//13
+//x +=1 and  x = x + 1 --> they are equivalent but x +=1 is shorter
+
+//14
+
+let y = 5
+while(y !=0){
+console.log(y)
+y--
+}
+//15
+
+
