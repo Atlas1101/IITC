48aea1bece5fceeb47da22baea1e64da4501752a
Aharon Cohen <ayc@live.co.uk>
2024-07-27 19:27:21 +0300

    6/9

---
+++ 2024/07/25.07/HW.F/task 5 data types/main.js
@@ -0,0 +1,94 @@
+//1 - An integer/number
+
+
+//2 ``,"",''
+
+//3 - Undefined is a variable without a value/ a variable with the value undefined ie person= undefined =both its value and type are undefined
+
+//Null is an object type that represents nothing ie person = null = the value is nothing but person is still an object
+
+//4 type of
+console.log(typeof "John") ;
+console.log( typeof 3.14) ;
+console.log( typeof true)  ; 
+
+//5  
+console.log( typeof 5 /2)
+//it processes type of 5 first = number --> number/2 = Nan
+
+//6 boolean
+
+//7 
+
+person ={name:"maurice"}
+let anotherPerson = new Object()
+
+//8 Object
+
+//9 ==checks value, === checks value and type
+
+//10 
+
+let string = Number("1000000")
+let stringA =parseFloat("123")
+let stringB =parseInt("456")
+let stringC = "123123"
+console.log(string);
+console.log(stringA);
+console.log(stringB);
+console.log(stringC);
+
+//11 -53
+console.log("5"+3)
+console.log("55"+3)
+
+//12
+console.log(typeof "John") ;
+console.log( typeof 3.14) ;
+console.log( typeof true)  ; 
+
+
+//13 - multi line
+let text =
+`The quick
+brown fox
+jumps over
+the lazy dog`;
+console.log(text);
+
+//14 single and double are the same backticks allow for  interpolation
+let firstName = "John";
+let lastName = "Doe";
+
+let textA = `Welcome ${firstName}, ${lastName}!`;
+console.log(textA);
+// and multiple line strings (see above)
+
+//15
+let x = 123;
+let str = String(x);
+console.log(str);
+
+let y =456
+let z = y.toString();
+console.log(z);
+
+//16- see the answe to question 3
+
+//17
+const likeLet ="But is an unchangeable variable"
+
+//18 see question 11
+
+//19 boolean
+
+//20 \' = '    \" = "       \\ =\
+
+//21 let has to be declared before use and can be re-declared (but not in the same scope) Var can be re-declared regardless of the scope. Const can not be re=declared no matter the scope.
+
+//22 use type of
+
+//23 105
+//24       3<2
+
+//25 primitive data types eg  numbers string booleans undefined cannot be change (?) they are compared by value. Objects are complex and they can hold multiple values such as arrays object literals and functions
