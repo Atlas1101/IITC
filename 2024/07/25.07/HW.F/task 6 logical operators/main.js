33bd5a8e6e80ac0ae516bfe7fc3721dcac427ca3
Aharon Cohen <ayc@live.co.uk>
2024-07-27 19:39:09 +0300

    7/9

---
+++ 2024/07/25.07/HW.F/task 6 logical operators/main.js
@@ -0,0 +1,40 @@
+//1  && ! ||
+
+//2 It returns true only if both operands are true
+
+//3 true
+console.log(true && true)
+
+//4 =false
+
+//5 It returns true if at least one operand is true.
+
+//6 true
+
+//7 false
+
+//8 It inverts the boolean value of its operand. 
+
+//9 false
+
+//10 true
+
+//11 false
+
+//12 true
+
+//13 not -> and -> or
+
+//14 It's when the second operand is not evaluated if the first operand is sufficient to determine the result
+
+//15 when a is sufficient to determine the result
+
+//16 same as 15
+
+//17 true
+
+//18 false
+
+//19 false
+
+//20 it decides what is evaluated first
