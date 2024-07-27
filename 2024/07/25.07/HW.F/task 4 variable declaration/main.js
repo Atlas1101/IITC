6b250d990bec90cd213af3b84fe6ccc3d0317dcb
Aharon Cohen <ayc@live.co.uk>
2024-07-27 16:17:39 +0300

    almost there

---
+++ 2024/07/25.07/HW.F/task 4 variable declaration/main.js
@@ -0,0 +1,95 @@
+//1
+let firstName = "Aharon ";
+console.log(firstName);
+
+//2
+let age = 26;
+
+console.log(age);
+
+//3
+let isRaining = false;
+
+console.log(isRaining);
+
+//4
+const pi = 3.14159;
+
+//5
+let isStudent = true;
+
+console.log(isStudent);
+
+//6
+
+//let favouriteColour = 
+
+//7
+let temperature = 72.5;
+
+//8  convention to use uppercase for constant names
+const DAYS_IN_WEEK = 7;
+
+//9
+
+let  username = "code123";
+
+
+//10
+let score = 0
+console.log(score);
+
+//11
+let isLoggedIn = false
+console.log(isLoggedIn);
+
+//12
+const  COMPANY_NAME = "Tech Solutions Inc"
+console.log(COMPANY_NAME);
+
+
+//13
+
+console.log(Date())
+let currentYear =  2024;
+console.log(currentYear);
+
+//14
+let price = 19.99
+console.log(price);
+
+//15
+let quantity =100
+console.log(quantity);
+
+//16
+let counter = 0
+counter ++
+console.log(counter);
+
+//17
+
+let message = "Hello"
+message = "Hello World"
+console.log(message);
+
+
+//18
+let myVariable = 42
+myVariable = "forty-two"
+console.log(myVariable);
+
+
+
+
+//19
+let totalCost = 50
+totalCost += 10
+console.log(totalCost);
+
+//20
+
+
+let usernameA = "user1"
+usernameA = "admin"
+console.log(usernameA);
