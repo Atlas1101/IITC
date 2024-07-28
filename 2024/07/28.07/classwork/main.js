4d0eb2b1010ead5e52d08265c9cf52ea60772263
Aharon Cohen <ayc@live.co.uk>
2024-07-28 14:54:46 +0300

    update

---
+++ 2024/07/28.07/classwork/main.js
@@ -0,0 +1,76 @@
+//for(let i=10; i>=0; i-- )
+//console.log(i);
+
+//for(let i=0; i<=20; i+=2)
+//console.log(i);
+
+
+// let sum=0
+// for(let i=0; i<=10; i++){
+//     sum+=i}
+// console.log(sum)
+
+
+// for(let i=5; i<=50; i+=5){
+//    console.log(i) 
+// }
+
+// line= "*"
+// for(let i="*"; i.length <= 6; i+="*"){
+//     line+=i
+// console.log(i)
+// }
+
+// line= " * "
+// for(let i="*"; i.length <= 9; i+="**"){
+//     line+=i}
+// new line
+// for(let space="     "; space.length>=4;space.slice(0,-1)){
+//     console.log(space)
+// }
+
+
+
+//     let finalLine = space + i
+// console.log(finalLine)
+// }
+// let f = 1
+// while (f<50){
+    
+//     f*=2
+//     console.log(f);
+// }
+
+// let f = 1
+// while (f<100){
+    
+//     console.log(f);
+//     f*=2
+// 
+
+// let number = ""
+// let i =0
+// do {
+//     number +=i
+//     i++
+// ;
+// }
+// while (i<5);
+// console.log(number);= 1
+
+
+
+
+// let i =1
+// do{
+// console.log(i++);
+
+// }while (i <=5)
+
+let option = ""
+let answer =""
+do{
+
+
+
+}
