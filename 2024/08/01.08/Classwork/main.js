6f6523f6c3e0d71af315db081098a58af6535ff0
Aharon Cohen <ayc@live.co.uk>
2024-08-01 11:00:15 +0300

    one step

---
+++ 2024/08/01.08/Classwork/main.js
@@ -0,0 +1,38 @@
+console.log("test");
+//1
+for(let i = 1;i<=5;i++){
+    let thing = ""
+
+    for(j=1;j<=i;j++){
+        thing +="*";
+    }
+    console.log(thing);
+}
+
+//2
+
+for (let i = 1; i<=10; i++){
+    let row = "" 
+    for(j=1;j<=10;j++){
+        row += i*j+" "
+
+
+        
+        
+    }
+   row += "\n"
+
+   console.log(row);
+}
+
+//3
+
+let arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+for (let i = 1; i<=arr.length; i++){
+    let row =
+    for(j=1; j<=i; j++){
+        if
+    }
+    log row
+}
