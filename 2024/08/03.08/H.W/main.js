83e717f5c22f3b4d6514f97f6abdfe593826c377
Aharon Cohen <ayc@live.co.uk>
2024-08-03 17:46:16 +0300

    progress

---
+++ 2024/08/03.08/H.W/main.js
@@ -0,0 +1,399 @@
+//loops level 0
+
+
+//1
+
+
+// function counter(){
+
+// for(let i =1; i<=5;i++){
+    
+//     console.log(i);
+// }
+// }
+// counter()
+
+
+//2
+
+
+
+// function counter(){
+
+// for(let i =0; i<=9;i++){
+    
+//     console.log(i);
+// }
+// }
+// counter()
+
+
+
+//3
+
+
+// function counter(){
+
+// for(let i =10; i>0;i--){
+    
+//     console.log(i);
+// }
+// }
+// counter()
+
+
+//4
+
+
+// function counter(){
+
+// for(let i =2; i<=10;i+=2){
+    
+//     console.log(i);
+// }
+// }
+// counter()
+
+//5
+
+
+// function counter(){
+
+// for(let i =1; i<=10;i+=2){
+    
+//     console.log(i);
+// }
+// }
+// counter()
+
+
+//6
+
+// function counter(){
+
+// for(let i =0; i<=15;i+=3){
+    
+//     console.log(i);
+// }
+// }
+// counter()
+
+//7
+// function counter(){
+
+// for(let i =2; i<=20;i+=2){
+    
+//     console.log(i);
+// }
+// }
+// counter()
+
+
+
+
+//8
+
+// function counter(){
+
+// for(let i =10; i>0;i-=2){
+    
+//     console.log(i);
+// }
+// }
+// counter()
+
+
+
+
+//9
+//10
+//11
+
+
+// function counter(){
+// let row = ""
+// for(let i =0; i<5;i++){
+//     row += "*"
+  
+// }
+// console.log(row);
+// }
+// counter()
+
+
+
+
+
+
+
+
+
+
+
+
+
+//12
+// function counter(){
+// let row = ""
+// for(let i =0; i<3;i++){
+//     row += "hello" +"\n"
+  
+// }
+// console.log(row);
+// }
+// counter()
+
+
+
+
+//13
+
+// function counter(){
+// let row = ""
+// for(let i =1; i<=3;i++){
+//     row += i +"!"
+  
+// }
+// console.log(row);
+// }
+// counter()
+
+
+
+
+//14
+// function counter(){
+// let str = "ABCDE"
+// for(let i =0; i<str.length;i++){
+//     let letter = str.charAt(i)
+  
+//     console.log(letter);
+// }
+// }
+// counter()
+
+
+//15
+// function counter(){
+// let row = ""
+// for(let i =1; i<=3;i++){
+//     row += 2 +" "
+  
+// }
+// console.log(row);
+// }
+// counter()
+
+
+
+
+
+//16
+
+// function counter(){
+// let numbers = [1, 2, 3, 4, 5]
+
+// for(let i =0; i<numbers.length;i++){
+//     let output= numbers[i]
+  
+//     console.log(output);
+// }
+// }
+// counter()
+
+
+//17
+// function counter(){
+// let numbers = ['a', 'b', 'c', 'd']
+
+// for(let i =0; i<numbers.length;i++){
+//     let output= numbers[i]
+  
+//     console.log(output);
+// }
+// }
+// counter()
+
+
+
+
+//18
+
+
+// function counter(){
+// let numbers = [10, 20, 30, 40, 50]
+
+// for(let i =numbers.length-1; i>=0;i--){
+//     let output= numbers[i]
+  
+//     console.log(output);
+// }
+// }
+// counter()
+
+
+
+
+
+//19
+
+
+// function counter(){
+// let numbers = [1, 2, 3, 4, 5, 6]
+
+// for(let i =1; i<numbers.length;i+=2){
+ 
+//     let output= numbers[i]
+//     console.log(output);
+// }
+// }
+// counter()
+
+
+
+
+
+//20
+
+
+// function counter(){
+// let numbers = ['red', 'green', 'blue']
+
+// for(let i =0; i<numbers.length;i++){
+ 
+//     let output= "colour " + numbers[i]
+//     console.log(output);
+// }
+// }
+// counter()
+
+//21
+
+
+
+
+// function counter(){
+// let numbers = ['red', 'green', 'blue']
+
+// for(let i =0; i<numbers.length;i++){
+ 
+//     let output= "colour " + numbers[i]
+//     console.log(output);
+// }
+// }
+// counter()
+
+
+
+//22
+// function counter(){
+// let sum = 0
+
+// for(let i =0; i<=5;i++){
+//     sum += i
+
+// }
+// console.log(sum);
+// }
+// counter()
+
+
+
+
+//23
+// function counter(){
+// let sum = 1
+
+// for(let i =1; i<=5;i++){
+//     sum *= i
+
+// }
+// console.log(sum);
+// }
+// counter()
+
+
+
+
+
+
+
+// real 23
+// function counter(){
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+// let count =0
+// for(let i =0; i<numbers.length;i++){
+//     if(numbers[i]%2 ===0){
+//         count +=1
+        
+//     }
+
+// }
+// console.log(count);
+// }
+// counter()
+
+
+
+
+
+
+
+
+
+//24
+// function counter(){
+// let numbers = [10, 5, 8, 12, 3]
+// let largest = numbers[0]
+// for(let i =0; i<numbers.length;i++){
+//     if(numbers[i]>largest){
+//         largest =numbers[i]
+        
+//     }
+
+// }
+// console.log(largest);
+// }
+// counter()
+
+
+
+//25
+
+// function counter(){
+// let sum =0
+// for(let i =0; i<=10;i++){
+//     if(i%2 ===0){
+       
+//         sum +=i
+//     }
+
+// }
+// console.log(sum);
+// }
+// counter()
+
+
+
+
+// 26
+
+function counter(){
+let row =""
+for(let i =0; i<=2;i++){
+    
+    for(let j =0; j<=i;j++)
+}
+console.log(sum);
+}
+counter()
+
+
+
+//
+//
+//
+//
+//
+
+
