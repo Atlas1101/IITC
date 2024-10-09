dc83bad628935070e51843fd743e2c79b45c7fec
Aharon Cohen <ayc@live.co.uk>
2024-10-09 10:48:35 +0300

    ok

--- 2024/08/01.08/Classwork/main.js
+++ 2024/08/01.08/Classwork/main.js
@@ -1,77 +1,77 @@
 console.log("test");
 //1
-for(let i = 1;i<=5;i++){
+for (let i = 1; i <= 5; i++) {
     let thing = ""
 
-    for(j=1;j<=i;j++){
-        thing +="*";
+    for (j = 1; j <= i; j++) {
+        thing += "*";
     }
     console.log(thing);
 }
 
 //2
 
-for (let i = 1; i<=10; i++){
-    let row = "" 
-    for(j=1;j<=10;j++){
-        row += i*j+" "
+for (let i = 1; i <= 10; i++) {
+    let row = ""
+    for (j = 1; j <= 10; j++) {
+        row += i * j + " "
 
 
-        
-        
+
+
     }
-   row += "\n"
+    row += "\n"
 
-   console.log(row);
+    console.log(row);
 }
 
 //3 when setting loop condition with arrays, dont use <= only < as arrays are zero based index so the loop would run an extra time on <= and give an out of bounds error
-function finder(target){
-let arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]; 
+function finder(target) {
+    let arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 
-for (let i = 0; i<arr.length; i++){
-  
-   
-    for(j=0; j<arr[i].length; j++){
-         if (arr[i][j] === target){
-            console.log(`${target} = found at position :[${i}] [${j}]`);
-            return
-         }
+    for (let i = 0; i < arr.length; i++) {
+
+
+        for (j = 0; j < arr[i].length; j++) {
+            if (arr[i][j] === target) {
+                console.log(`${target} = found at position :[${i}] [${j}]`);
+                return
+            }
+        }
+
     }
-    
-}
 }
 finder(8)
 
 //4 
-function letterSorter(input){
+function letterSorter(input) {
     let vowels = "aeiouAEIOU"
     let vowelCount = 0;
     let consonantCount = 0;
-    
-    for (let i = 0; i<input.length; i++){
+
+    for (let i = 0; i < input.length; i++) {
         let letter = input[i]
-        let isVowel= false
+        let isVowel = false
         //console.log(input[i]);
-        
-        for(j=0; j<vowels.length; j++){
+
+        for (j = 0; j < vowels.length; j++) {
             //console.log(vowels[j]);
             let vowelLetter = vowels[j]
-            if (letter === vowelLetter){
+            if (letter === vowelLetter) {
                 vowelCount += 1
                 isVowel = true
                 break
-            
-            }
-            
-            
 
-    }
-    if (!isVowel){
-                consonantCount += 1
             }
-}
-console.log(`vowel count = ${vowelCount} consonanat count = ${consonantCount}`);
+
+
+
+        }
+        if (!isVowel) {
+            consonantCount += 1
+        }
+    }
+    console.log(`vowel count = ${vowelCount} consonanat count = ${consonantCount}`);
 }
 letterSorter("abarakadabraaa")
 console.log("sdfsd");
@@ -91,40 +91,40 @@ console.log("sdfsd");
 
 
 //1
-    for (let i = 0; i<1; i++){
+for (let i = 0; i < 1; i++) {
 
-        let row= ""
+    let row = ""
 
 
-        for(j=0;j<=1;j++){
+    for (j = 0; j <= 1; j++) {
 
-        row ="* *"
+        row = "* *"
 
         console.log(row);
-}
- 
-}
-
-
-function gridBuilder(g){
-
-
-    let grid= ""
-
-
- for (let i = 0; i<=g; i++){
-
-
-
-        for(j=0;j<=g;j++){
-
-        grid =" * "
-
     }
-    
-    grid += "\n"
-    console.log(gridBuilder);
+
 }
+
+
+function gridBuilder(g) {
+
+
+    let grid = ""
+
+
+    for (let i = 0; i <= g; i++) {
+
+
+
+        for (j = 0; j <= g; j++) {
+
+            grid = " * "
+
+        }
+
+        grid += "\n"
+        console.log(gridBuilder);
+    }
 }
 gridBuilder(10)
 
@@ -132,19 +132,19 @@ gridBuilder(10)
 
 //2
 
-    for (let i = 0; i<=1; i++){
+for (let i = 0; i <= 1; i++) {
 
-        let row=""
+    let row = ""
 
 
 
-        for(j=0;j<2;j++){
-            let counter =i+1
-        row +=1
+    for (j = 0; j < 2; j++) {
+        let counter = i + 1
+        row += 1
 
-        console.log(counter+row);
-}
- 
+        console.log(counter + row);
+    }
+
 }
 
 
